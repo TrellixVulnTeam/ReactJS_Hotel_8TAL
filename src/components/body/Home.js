8bf4f7300d02da946b68162076f2fc540f438dbf
longnguyen-2k <longnguyen29122k@gmail.com>
2021-05-15 19:00:00 +0700

    Item_home

--- src/components/body/Home.js
+++ src/components/body/Home.js
@@ -1,52 +1,19 @@
 import React, { Component } from 'react';
-
+import Item_Home from '../Item/Item_home'
 class Home extends Component {
 
-    render() {
-        return (
-            <div>
-        <div className="container">
-          <form className="booking-form">
-            <div className="arrive">
-              <label htmlFor="arrive">Arrive</label>
-              <input id="arrive" type="date" />
-            </div>
-            <div className="departure">
-              <label htmlFor="departure">Departure</label>
-              <input id="departure" type="date" />
-            </div>
-            <div className="select-book">
-              <label>Person</label>
-              <select name="book" id="person" className>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-              </select>
-            </div>
-            <div className="select-book">
-              <label>Type room</label>
-              <select name="book" className>
-                <option value="luxury">Luxury</option>
-                <option value="family">Family</option>
-                <option value="couple">Couple </option>
-                <option value="standard">Standard </option>
-              </select>
-            </div>
-            <div className="submit-form">
-              <input type="submit" defaultValue="Check Availability" />
-            </div>
-          </form>
-        </div>
+  render() {
+    return (
+      <div>
         <p>
         </p><div className="container ">
           <center>
-            <h1 style={{fontFamily: 'Didot', color: '#CC6600'}}>WELCOME TO HKTQUEEN HOTEL</h1>
+            <h1 style={{ fontFamily: 'Didot', color: '#CC6600' }}>WELCOME TO HKTQUEEN HOTEL</h1>
           </center><br />
           <div className="row infor_1 ">
-            <div className="col-md-5 " style={{fontSize: '22px', fontFamily: 'Optima', color: '#003300'}}>HKT Queen Hotel Đà Nẵng City is a world-class 5-star hotel, located at the intersection of four main districts, which is considered the heart and center of the city Đà Nẵng. It takes you only 35 minutes drive from the hotel to Đà Nẵng
-              airport, 8 minutes to the commercial and entertainment center of the city. With 300 elegantly designed and elegant standard rooms and suites with attention to every detail, it will bring the utmost comfort and comfort to you whether
-              it is a relaxing time or a business trip. </div>
+            <div className="col-md-5 " style={{ fontSize: '22px', fontFamily: 'Optima', color: '#003300' }}>HKT Queen Hotel Đà Nẵng City is a world-class 5-star hotel, located at the intersection of four main districts, which is considered the heart and center of the city Đà Nẵng. It takes you only 35 minutes drive from the hotel to Đà Nẵng
+            airport, 8 minutes to the commercial and entertainment center of the city. With 300 elegantly designed and elegant standard rooms and suites with attention to every detail, it will bring the utmost comfort and comfort to you whether
+                it is a relaxing time or a business trip. </div>
             <div className="col-md-7 ">
               <div className="thumbnail">
                 <img className="card-img-top " src="images/home/introduction1.jpg" alt="HKTQueen" />
@@ -61,7 +28,7 @@ class Home extends Component {
           <div className="row mt-5 ">
             <h2 className="list-product-title ">
               <center>
-                <h1 style={{fontFamily: 'Didot', color: '#CC6600'}}>ROOM</h1>
+                <h1 style={{ fontFamily: 'Didot', color: '#CC6600' }}>ROOM</h1>
               </center>
             </h2>
           </div>
@@ -73,122 +40,21 @@ class Home extends Component {
             <div className="carousel-inner">
               <div className="carousel-item active">
                 <div className="row ">
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Presidential Room</h5>
-                        <p className="card-text ">Luxurious design, noble, comfortably.</p>
-                        <center>
-                          <a href="ViewDetailsPresidential.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Deluxe Room</h5>
-                        <p className="card-text ">The room is quite large, fully equipped and comfortable.</p>
-                        <center>
-                          <a href="ViewDetailsDeluxe.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Superior Room</h5>
-                        <p className="card-text ">Full device and comfort, balcony overlooking the pool.</p>
-                        <center>
-                          <a href="ViewDetailsSuperior.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Standard Room</h5>
-                        <p className="card-text ">Reasonable price, simple design, suitable.</p>
-                        <center>
-                          <a href="ViewDetailsStandard.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Luxury" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Family" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Luxury" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Couple" des=""></Item_Home>
+
+
+
                 </div>
               </div>
               <div className="carousel-item room2">
-                <div className="row ">
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Presidential Room</h5>
-                        <p className="card-text ">Luxurious design, noble, comfortably.</p>
-                        <center>
-                          <a href="ViewDetailsPresidential.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Deluxe Room</h5>
-                        <p className="card-text ">The room is quite large, fully equipped and comfortable.</p>
-                        <center>
-                          <a href="ViewDetailsDeluxe.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Superior Room</h5>
-                        <p className="card-text ">Full device and comfort, balcony overlooking the pool.</p>
-                        <center>
-                          <a href="ViewDetailsSuperior.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="col-md-3 col-sm-6 col-12">
-                    <div className="card card-product mb-3 ">
-                      <div className="thumbnail">
-                        <img className="card-img-top " src="images/home/Presidential1.jpg" alt="HKTQueen" />
-                      </div>
-                      <div className="card-body ">
-                        <h5 className="card-title ">Standard Room</h5>
-                        <p className="card-text ">Reasonable price, simple design, suitable.</p>
-                        <center>
-                          <a href="ViewDetailsStandard.php" className="btn btn-primary ">View Details</a>
-                        </center>
-                      </div>
-                    </div>
-                  </div>
+                <div className="row">
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Luxury" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Luxury" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Standard" des=""></Item_Home>
+                  <Item_Home image="images/home/Presidential1.jpg" roomtype="Luxury" des=""></Item_Home>
                 </div>
               </div>
             </div>
@@ -202,9 +68,51 @@ class Home extends Component {
             </a>
           </div>
         </div>
+        {/* end slide room*/}
+        {/* Load jquery trước khi load bootstrap js */}
+        <br />
+        <hr />
+        <div className="container ">
+          <center>
+            <h1 style={{ fontFamily: 'Didot', color: '#CC6600' }}>DERVICES</h1>
+          </center>
+          <br />
+          <div className="row infor_2 ">
+            <div className="col-md-7 ">
+              <img className="card-img-top " src="images/home/introduction2.jpg" alt="HKTQueen" />
+            </div>
+            <div className="col-md-5 " style={{ fontSize: '22px', fontFamily: 'Optima', color: '#003300' }}>The hotel has Orientica Seafood Restaurant &amp; Bar and Café Chat. Modern quality facilities of meeting and conference rooms can accommodate up to 1250 people. In addition, the hotel also has a world-class sports center with outdoor temperatures
+            (floating bars on the water), shaping services, spa and massage. Hotel HTK Queen Đà Nẵng City with excellent facilities and services is the ideal choice for clients on jobs and vacations before visiting the most vibrant and rhythmic
+                city in Vietnam.</div>
+          </div><br />
+          <div className="row infor_2">
+            <div className="col-sm-3">
+              <div className="thumbnail">
+                <img className="card-img-top " src="images/home/restaurant.jpg" alt="HKTQueen" />
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="thumbnail">
+                <img className="card-img-top " src="images/home/spa.jpg" alt="HKTQueen" />
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="thumbnail">
+                <img className="card-img-top " src="images/home/hoboi.jpg" alt="HKTQueen" />
+              </div>
+            </div>
+            <div className="col-sm-3">
+              <div className="thumbnail">
+                <img className="card-img-top " src="images/home/gym.jpg" alt="HKTQueen" />
+              </div>
+            </div>
+          </div>
+        </div>
+        <br />
+        <hr />
       </div>
-        );
-    }
+    );
+  }
 }
 
 export default Home;
