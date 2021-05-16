ce81e5a2dda489d406da9f51d7eb237bccc176e0
intern.thanh <thanh.codecomplete@gmail.com>
2021-05-16 12:20:18 +0700

    router to rooms again nha

--- src/components/router/Router_rooms/room.js
+++ src/components/router/Router_rooms/room.js
@@ -1,5 +1,6 @@
 
 import React, { Component } from 'react'
+import Item from'./item';
 import '../css/room.css';
 
 class Room extends Component {
@@ -10,90 +11,10 @@ class Room extends Component {
               <div className="container">
                 <div className="wrap-room-4">
                   <div className="row">
-                    <div className="wrap-item clearfix">
-                      <div className="col-xs-6 col-sm-5 col-md-5 col-lg-5">
-                        <div className="img">
-                          <img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-18.jpg" alt="#" className="img-responsive" />
-                        </div>
-                      </div>
-                      <div className="col-xs-6 col-sm-7 col-md-7 col-lg-7">
-                        <div className="text-room-4">
-                          <div className="text">
-                            <h2 className="h2-rooms">LUXURY ROOM</h2>
-                            <h5 className="h5-room">Start form $120 per day</h5>
-                            <p>Lorem Ipsum is simply dummy text of the <b>printing</b> and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <b>the 1500s</b>, when an unknown printer took a <b>gallery</b>                                            of type and scrambled it to make a type specimen book ...</p>
-                            <a href="room-detail.php" className="view-dateails btn btn-room">VIEW DETAILS</a>
-                            <div className="line" />
-                          </div>
-                          <div className="img1">
-                            <a href="room-detail.php"><img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-19.jpg" alt="#" className="img-responsive" /></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="wrap-item clearfix">
-                      <div className="col-xs-6 col-sm-5 col-md-5 col-lg-5 col-lg-push-7 col-md-push-7 col-sm-push-7 col-xs-push-6">
-                        <div className="img">
-                          <img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-20.jpg" alt="#" className="img-responsive" />
-                        </div>
-                      </div>
-                      <div className="col-xs-6 col-sm-7 col-md-7 col-lg-7 col-lg-pull-5 col-md-pull-5 col-sm-pull-5 col-xs-pull-6">
-                        <div className="text-room-4 room-right">
-                          <div className="text">
-                            <h2 className="h2-rooms">FAMILY ROOM</h2>
-                            <h5 className="h5-room">Start form $120 per day</h5>
-                            <p>Lorem Ipsum is simply dummy text of the <b>printing</b> and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <b>the 1500s</b>, when an unknown printer took a <b>gallery</b>                                            of type and scrambled it to make a type specimen book ...</p>
-                            <a href="room-detail.php" className="view-dateails btn btn-room">VIEW DETAILS</a>
-                            <div className="line" />
-                          </div>
-                          <div className="img1">
-                            <a href="room-detail.php"><img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-21.jpg" alt="#" className="img-responsive" /></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="wrap-item clearfix">
-                      <div className="col-xs-6 col-sm-5 col-md-5 col-lg-5">
-                        <div className="img">
-                          <img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-22.jpg" alt="#" className="img-responsive" />
-                        </div>
-                      </div>
-                      <div className="col-xs-6 col-sm-7 col-md-7 col-lg-7">
-                        <div className="text-room-4">
-                          <div className="text">
-                            <h2 className="h2-rooms">COUPLE ROOM</h2>
-                            <h5 className="h5-room">Start form $120 per day</h5>
-                            <p>Lorem Ipsum is simply dummy text of the <b>printing</b> and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <b>the 1500s</b>, when an unknown printer took a <b>gallery</b>                                            of type and scrambled it to make a type specimen book ...</p>
-                            <a href="room-detail.php" className="view-dateails btn btn-room">VIEW DETAILS</a>
-                            <div className="line" />
-                          </div>
-                          <div className="img1">
-                            <a href="room-detail.php"><img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-23.jpg" alt="#" className="img-responsive" /></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div className="wrap-item clearfix">
-                      <div className="col-xs-6 col-sm-5 col-md-5 col-lg-5 col-lg-push-7 col-md-push-7 col-sm-push-7 col-xs-push-6">
-                        <div className="img">
-                          <img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-24.jpg" alt="#" className="img-responsive" />
-                        </div>
-                      </div>
-                      <div className="col-xs-6 col-sm-7 col-md-7 col-lg-7 col-lg-pull-5 col-md-pull-5 col-sm-pull-5 col-xs-pull-6">
-                        <div className="text-room-4 room-right">
-                          <div className="text">
-                            <h2 className="h2-rooms">STANDARD ROOM</h2>
-                            <h5 className="h5-room">Start form $120 per day</h5>
-                            <p>Lorem Ipsum is simply dummy text of the <b>printing</b> and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since <b>the 1500s</b>, when an unknown printer took a <b>gallery</b>                                            of type and scrambled it to make a type specimen book ...</p>
-                            <a href="room-detail.php" className="view-dateails btn btn-room">VIEW DETAILS</a>
-                            <div className="line" />
-                          </div>
-                          <div className="img1">
-                            <a href="room-detail.php"><img src="http://landing.engotheme.com/html/skyline/demo/images/Room/room-25.jpg" alt="#" className="img-responsive" /></a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
+                        <Item image1="http://landing.engotheme.com/html/skyline/demo/images/Room/room-18.jpg" roomtype="LUXURY ROOM" pri="Start form $120 per day" image2="http://landing.engotheme.com/html/skyline/demo/images/Room/room-19.jpg" des=" Lorem Ipsum has been the industry's standard dummy text ever since.the 1500s, when an unknown printer took a gallery                                           of type and scrambled it to make a type specimen book ..."></Item>
+                        <Item image1="http://landing.engotheme.com/html/skyline/demo/images/Room/room-20.jpg" roomtype="FAMILY ROOM" pri="Start form $150 per day" image2="http://landing.engotheme.com/html/skyline/demo/images/Room/room-21.jpg" des="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery                                          of type and scrambled it to make a type specimen book ..."></Item>
+                        <Item image1="http://landing.engotheme.com/html/skyline/demo/images/Room/room-22.jpg" roomtype="COUPLE ROOM" pri="Start form $150 per day" image2="http://landing.engotheme.com/html/skyline/demo/images/Room/room-23.jpg" des="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery                                          of type and scrambled it to make a type specimen book ..."></Item>
+                        <Item image1="http://landing.engotheme.com/html/skyline/demo/images/Room/room-24.jpg" roomtype="STANDARD ROOM" pri="Start form $170 per day"image2="http://landing.engotheme.com/html/skyline/demo/images/Room/room-25.jpg" des="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a gallery                                          of type and scrambled it to make a type specimen book ..."></Item>
                   </div>
                 </div>
               </div>
@@ -102,3 +23,4 @@ class Room extends Component {
 }
 }
 export default Room;
+ 
