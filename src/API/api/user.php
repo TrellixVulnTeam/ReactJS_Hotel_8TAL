af8577b7b90040d9c9d4e17d0e9854c9c122133d
intern.thanh <thanh.codecomplete@gmail.com>
2021-05-18 01:26:11 +0700

    Class user làm lại

--- src/API/api/user.php
+++ src/API/api/user.php
@@ -28,38 +28,39 @@ class User extends restful_api
 		}elseif ($this->method == 'POST') {
 			$conn = new Database();
 			if(empty($this->params)){
-				$con->query("INSERT INTO users (name, email, password, code, status, role)
-				             values('$name', '$email', '$password', '$code', '$status', '$user')");
-			}else{
-				$data = $con->getData();
+					$this->response(500,"Khong co thong tin");
+			}else{	
+				$con->query("INSERT INTO users (name, email, password)
+				             values('$name', '$email', '$password'");
 			}
+			$this->response(200,$data);
 
 		} elseif ($this->method == 'PUT') {
-			if (empty($id = $this->params['id'])) {
-				$this->response(404, "Not found");
+			if (empty($id = $this->params['user_id'])) {
+				$this->response(404, "Khong tim thay id");
 			} else {
 				$con = new Database;
-				 $con->query("UPDATE users set name='$name',email='$email',
-				 password='$password',status='$st0atus',role='$role'
-				 where id='$id'");
+				 $con->query("UPDATE users set name='$name',email='$email',password='$password'			
+				 where user_id='$id'");
 				$con->close();
 				$this->response(200);
 			}
 
 		} elseif ($this->method == 'DELETE') {
-				$id = $this->params['id']; 
+				$id = $this->params['user_id']; 
 			if (empty($id)) {
-				$this->response(404, "Not found");
+				$this->response(404, "Khong tim thay id");
 			} else {
 				$con = new Database;
-				$con->query("SELECT * from users where id='$id'");
+				$con->query("SELECT * from users where user_id='$id'");
 				$data = $con->getData();
-				if ($data['status'] == 1)
+				if ($data['status'] == 1){
 					$status = 0; // 0 tuc la bi block
-				else
+				}else{
 					$status = 1; // 1 tuc la van con hoat dong		
-				$con->query("DELETE * from users where status='1'and id='$id'");// Còn hoạt động nên mới xóa đc pk?
+				$con->query("DELETE * from users where status='$status' where user_id='$id'");// Còn hoạt động nên mới xóa đc pk?
 				$this->response(200);
+				}
 			}
 		}
 	}
